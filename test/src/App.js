7740fad83cc0fef0d5d36b5159d8bb35cd9bea86
JosneyV16 <josneyn@gmail.com>
2022-02-15 16:04:24 -0500

    first commit

---
+++ test/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edita esta madre papa <code>src/App.js</code> guarda para que veas lo que hiciste.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React (aprende papaupa)
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
